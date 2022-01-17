059ba33941f623c0465cc83f425ce54d855dfbae
Gennady Badinov <gennady.badinov@gmail.com>
2022-01-17 12:08:37 +0300

    Add Polls app to installed apps list

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "polls.PollsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
