a6fb82563e48be423abf56732f7bfa536453f11a
Gennady Badinov <gennady.badinov@gmail.com>
2022-01-17 12:09:39 +0300

    Fix polls app in installed apps list

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "polls.PollsConfig",
+    "polls.apps.PollsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
